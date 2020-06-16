a7dfe8f5703f72df93aadfe7b816882bfe042a94
drachna <rachna.devasthali@gmail.com>
2020-06-16 21:47:27 +0530

    added lcs problems

---
+++ Dynamic_Programming/LCS/lcs.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+def lcs(s1,s2):
+    
+    dp=[x[:] for x in [[0]*(len(s1)+1)]*(len(s2)+1)]
+    #print(dp)
+    #dp[i][j] -- taking i letters from s2 and taking j letters from s1
+    #dp table starts index 1 but string starts from 0 so initialize row 0 col 0 with 0
+    for i in range(1,len(s2)+1):
+        for j in range(1,len(s1)+1):
+            if s2[i-1]==s1[j-1]:
+                dp[i][j]=1+dp[i-1][j-1]
+                
+            else:
+                #take max( leave either current element from s1 or leave current element of s2 )
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    print(dp)
+    print('max lcs',dp[-1][-1])
+                
+
+lcs("abcde","abcd")
+
+
+#leetcode problems on same problem
+#1. Uncrossed line
---
+++ Dynamic_Programming/LCS/longestCommonSubstring.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+def lcsubstring(s1,s2):
+    
+    dp=[x[:] for x in [[0]*(len(s1)+1)]*(len(s2)+1)]
+    
+    for i in range(1,len(s2)+1):
+        for j in range(1,len(s1)+1):
+            if s2[i-1]==s1[j-1]:
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=0
+    
+    print(dp)
+
+lcsubstring("abc","a")
+
